136d0b6df4dcdf13651d4fbbfc90f141d987d2a8
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-07 22:55:35 +0100

    fixed a typo

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -5,11 +5,58 @@ import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../services/auth.service';
 import * as firebase from 'firebase/app';
 
-import { ChatMessage } from '../models/chat-message.model';
+import { ChatMessage } from '../models/chat-message.models';
 
 @Injectable()
 export class ChatService {
+  user: any;
+  chatMessages: FirebaseListObservable<ChatMessage[]>;
+  chatMessage: ChatMessage;
+  userName: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) { 
+      this.afAuth.authState.subscribe(auth =>{
+       if (auth !== undefined && auth !== null){
+        this.user = auth;
+      }
+    });
+  }
+  sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    const email = this.user.email;
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      userName: this.userName,
+      email: email });
+
+      console.log('Called Sendmessage()')
+  }
+
+  getMessages(): FirebaseListObservable<ChatMessage[]>{
+    // query to create message feed binding
+    return this.db.list('messages', {
+      query: {
+        limitToLast: 25,
+        orderByKey: true
+      }
+    });
+  }
+
+  getTimeStamp() {
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' +
+                (now.getUTCMonth() +1) + '/' + 
+                now.getUTCDate();
+    const time = now.getUTCHours() + ':' +
+                now.getUTCMinutes() + ':' + 
+                now.getUTCSeconds();
+
+    return (date + ' ' + time);
+  }
 
 }
